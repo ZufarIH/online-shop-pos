fe3b24192b19fb16936b52f05a68d7c2b6314860
ZufarIH <zufar.zih@gmail.com>
2024-10-11 20:12:28 +0700

    nambah notif

--- controller/c_auth.js
+++ controller/c_auth.js
@@ -4,7 +4,10 @@ module.exports =
 {
 
     halaman_login: function(req,res) {
-        res.render('v_auth/login')
+        let data ={
+            notifikasi: req.query.notif,
+        }
+        res.render('v_auth/login', data)
     },
     proses_login: async function(req,res) {
         // ambil input dari halaman login
