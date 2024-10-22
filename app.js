ed8f7289e89d37ea31e2aa92ebadb6a61f245bec
ZufarIH <zufar.zih@gmail.com>
2024-10-22 11:32:18 +0700

    nambah

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const express   = require('express')
-const app       = express()
-const port      = 3000
-const c_beranda = require('./controller/c_beranda')
-const c_auth    = require('./controller/c_auth')
+const express       = require('express')
+const app           = express()
+const port          = 3000
+const cookieParser  = require('cookie-parser')
+const session       = require('express-session')
+const c_beranda     = require('./controller/c_beranda')
+const c_auth        = require('./controller/c_auth')
+const cek_login     = c_auth.cek_login
+const c_toko        = require('./controller/c_toko')
+const c_olshop      = require('./controller/c_olshop')
 
+// settingan untuk data session login
+app.use (cookieParser('secret'))
+app.use (session({
+    secret: 'rahasia',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1000*60)*30
+        // batas session expired
+        // 1000 milidetik *60 = 1 menit
+        // 1 menit * 30 = 1/2 jam
+    }
+}))
 
 app.use( express.urlencoded({extended:false}) )
 app.use( express.static('public') )
@@ -16,6 +34,14 @@ app.set('views', './view')
 app.get('/', c_beranda.halaman_awal)
 app.get('/auth/login', c_auth.halaman_login)
 app.post('/auth/proses-login', c_auth.proses_login)
+
+app.get('/toko', cek_login, c_toko.index)
+
+app.get('/olshop', cek_login, c_olshop.halaman_beranda)
+app.get('/olshop/produk', cek_login, c_olshop.halaman_index_produk)
+app.get('/olshop/produk/tambah', cek_login, c_olshop.halaman_form_tambah)
+app.post('/olshop/produk/proses-insert', cek_login, c_olshop.proses_insert_produk)
+
 
 app.listen(port, ()=>{
     console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
--- controller/c_auth.js
+++ controller/c_auth.js
@@ -1,13 +1,18 @@
+const bcrypt     = require('bcryptjs')
 const model_user = require('../model/m_user')
 
 module.exports =
 {
-
+    // if-nya biar object user gk bentrok
     halaman_login: function(req,res) {
+        if (req.session.user) {
+            res.redirect('/toko')
+        }else{
         let data ={
             notifikasi: req.query.notif,
         }
         res.render('v_auth/login', data)
+        }
     },
     proses_login: async function(req,res) {
         // ambil input dari halaman login
@@ -17,10 +22,28 @@ module.exports =
         let email_exist = await model_user.cari_email(form_email)
         if (email_exist.length > 0){
             // cek password
-            res.send('email ada')
+            let password_cocok = bcrypt.compareSync(form_password, email_exist[0].password)
+            if (password_cocok){
+                // arahkan ke halaman utama sistem
+                req.session.user = email_exist
+                res.redirect('/toko')
+            }else{
+                let pesan = 'password salah!'
+                res.redirect(`/auth/login?notif=${pesan}`)
+            }
         }else{
             // tendang kehalaman register
             let pesan = 'email belum terdaftar, silahkan registrasi lebih dulu'
+            res.redirect(`/auth/login?notif=${pesan}`)
+        }
+    },
+
+    cek_login: function(req,res,next){
+        if (req.session.user){
+            next()
+        }else{
+            // lempar kehalaman login
+            let pesan = 'sesi anda habis, silahkan masuk kembali!'
             res.redirect(`/auth/login?notif=${pesan}`)
         }
     },
