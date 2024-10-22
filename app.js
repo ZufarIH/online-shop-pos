27a5b95abb9d61ce885aae6d9bcfc1db479567eb
ZufarIH <zufar.zih@gmail.com>
2024-10-22 12:36:46 +0700

    sudah bisa upload produk

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app           = express()
 const port          = 3000
 const cookieParser  = require('cookie-parser')
 const session       = require('express-session')
+const fileUpload    = require('express-fileupload')
 const c_beranda     = require('./controller/c_beranda')
 const c_auth        = require('./controller/c_auth')
 const cek_login     = c_auth.cek_login
@@ -10,21 +11,23 @@ const c_toko        = require('./controller/c_toko')
 const c_olshop      = require('./controller/c_olshop')
 
 // settingan untuk data session login
-app.use (cookieParser('secret'))
-app.use (session({
+app.use( cookieParser('rahasia') )
+app.use( session({
     secret: 'rahasia',
     resave: true,
     saveUninitialized: false,
     cookie: {
-        maxAge: (1000*60)*30
-        // batas session expired
-        // 1000 milidetik *60 = 1 menit
+        maxAge: (1000 * 60) * 30
+        // batas session expired:
+        // 1000 milidetik * 60 = 1 menit
         // 1 menit * 30 = 1/2 jam
     }
 }))
 
+
 app.use( express.urlencoded({extended:false}) )
 app.use( express.static('public') )
+app.use( fileUpload() )
 
 
 app.set('view engine', 'ejs')
@@ -41,6 +44,7 @@ app.get('/olshop', cek_login, c_olshop.halaman_beranda)
 app.get('/olshop/produk', cek_login, c_olshop.halaman_index_produk)
 app.get('/olshop/produk/tambah', cek_login, c_olshop.halaman_form_tambah)
 app.post('/olshop/produk/proses-insert', cek_login, c_olshop.proses_insert_produk)
+app.get('/olshop/produk/detail/:id_produk', cek_login, c_olshop.detail_produk)
 
 
 app.listen(port, ()=>{
--- model/m_master_produk.js
+++ model/m_master_produk.js
@@ -4,23 +4,32 @@ const moment            = require('moment')
 const db                = config_database.db
 const eksekusi          = config_database.eksekusi
 
+
+
 module.exports =
 {
+
     getSemua: function() {
         let sqlSyntax = mysql.format(
-            `SELECT * FROM master_produk`
+            `SELECT
+                p.*, k.nama as kategori_nama 
+            FROM master_produk as p
+            LEFT JOIN master_produk_kategori as k ON k.id = p.kategori_id;`
         )
         return eksekusi( sqlSyntax )
     },
-    insert: function(req) {
+
+
+
+    insert: function(req, filename_foto1, filename_foto2, filename_foto3) {
         let sqlData = {
             nama            : req.body.form_nama,
             harga           : req.body.form_harga,
-            stock           : req.body.form_stock,
+            stok            : req.body.form_stok,
             kategori_id     : req.body.form_kategori,
-            foto1           : req.body.form_foto1,
-            foto2           : req.body.form_foto2,
-            foto3           : req.body.form_foto3,
+            foto1           : filename_foto1,
+            foto2           : filename_foto2,
+            foto3           : filename_foto3,
             video           : req.body.form_video,
             deskripsi       : req.body.form_deskripsi,
             spesifikasi     : req.body.form_spesifikasi,
@@ -28,6 +37,7 @@ module.exports =
             created_at      : moment().format('YYYY-MM-DD HH:mm:ss'),
             created_by      : req.session.user[0].id,
         }
+
         let sqlSyntax = mysql.format(
             `INSERT INTO master_produk SET ?`,
             [sqlData]
